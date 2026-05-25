da2fc16efb783aa0754013c53968a1f9b2d20978
RaniaAkramm <ssess535@gmail.com>
2026-05-25 12:56:52 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // هذا يستدعي الكود الذي أرسلته لي سابقاً
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
